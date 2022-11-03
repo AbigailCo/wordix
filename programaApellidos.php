1d5d765347a1427b197373152dcd429b7a54d6b3
Ariel-mamani <arielmamani764@gmail.com>
2022-11-03 15:44:38 -0300

    actualizacion

--- programaApellidos.php
+++ programaApellidos.php
@@ -6,13 +6,15 @@ include_once("wordix.php");
 /**************************************/
 
 /* Apellido, Nombre. Legajo. Carrera. mail. Usuario Github */
-/* ... COMPLETAR ... */
+/*Mamani Ariel Maximiliano. LEGAJO 3607. Tec en desarrollo web. Github Ariel-Mamani */
+/*Corrales Abigail Candelaria. LEGAJO 4251. Tec en desarrollo web. Github AbigailCo */
 
 
 
 /**************************************/
 /***** DEFINICION DE FUNCIONES ********/
 /**************************************/
+
 
 /**
  * Obtiene una colección de palabras
@@ -23,8 +25,8 @@ function cargarColeccionPalabras()
     $coleccionPalabras = [
         "MUJER", "QUESO", "FUEGO", "CASAS", "RASGO",
         "GATOS", "GOTAS", "HUEVO", "TINTO", "NAVES",
-        "VERDE", "MELON", "YUYOS", "PIANO", "PISOS"
-        /* ... COMPLETAR ... Agregar 5 palabras más */
+        "VERDE", "MELON", "YUYOS", "PIANO", "PISOS",
+        "AMIGOS","PERRO", "MADRE", "COSMO", "GATO"
     ];
 
     return ($coleccionPalabras);
@@ -45,6 +47,47 @@ function cargarColeccionPalabras()
 
 
 //Proceso:
+function seleccionarOpcion(){
+     echo "          MENU PARA JUGAR WORDIX       \n";
+    echo "1- Jugar al wordix con una palabra alegida \n";
+    echo "2- Jugar al wordix con una palabra aleatoria \n";
+    echo "3- Mostrar una partida \n";
+    echo "4- Mostrar partida ganadora \n";
+    echo "5- Mostrar resumen de jugador\n";
+    echo "6- Mostrar listado de partidas ordenadas por jugador y por palabra \n";
+    echo "7- Agregar una palabra de 5 letras \n";
+    echo "8- Salir \n";
+
+    $opcion=solicitarNumeroEntre(1,8);
+    return $opcion
+}
+
+if($solicitaNum==1){
+    $min=1;
+    $max=20; //se relaciona con el punto 7
+    echo "ingrese su nombre: ";
+    $nombre=trim(fgets(STDIN));
+    $numeroPalabra=solicitarNumeroEntre($min,$max);
+    $palabraWordix=cargarColeccionPalabras();
+    $partida = jugarWordix($palabraWordix[$numeroPalabra-1], strtolower($nombre));
+
+
+}elseif($solicitaNum==2){
+    
+}elseif($solicitaNum==3){
+    
+}elseif($solicitaNum==4){
+    
+}elseif($solicitaNum==5){
+    
+}elseif($solicitaNum==6){
+    
+}elseif($solicitaNum==7){
+    
+}elseif($solicitaNum==8){
+    
+}
+
 
 $partida = jugarWordix("MELON", strtolower("MaJo"));
 //print_r($partida);
--- wordix.php
+++ wordix.php
@@ -32,6 +32,7 @@ const ESTADO_LETRA_PERTENECE = "pertenece";
 function solicitarNumeroEntre($min, $max)
 {
     //int $numero
+    echo "Eliga un numero: ";
     $numero = trim(fgets(STDIN));
     while (!is_int($numero) && !($numero >= $min && $numero <= $max)) {
         echo "Debe ingresar un número entre " . $min . " y " . $max . ": ";
